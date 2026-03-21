fbd26e68d9257a716a3d8bbadb96b2bdb0f0c0d6
Gusty <a0985209465@gmail.com>
2026-03-21 22:01:48 +0800

    Update routing.ts

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,7 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['zh-TW', 'en', 'ja'],
   defaultLocale: 'zh-TW',
-  localeDetection: true, // 根據瀏覽器語系自動偵測
+  localeDetection: false, // 根據瀏覽器語系自動偵測
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
